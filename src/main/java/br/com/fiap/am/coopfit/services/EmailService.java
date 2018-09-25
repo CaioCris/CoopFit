818b0019216b820ebf3c791a9f1e0f3ebe0c5b9f
CaioCris <caiohcris@gmail.com>
2018-09-25 12:31:13 -0300

    hot fix

--- src/main/java/br/com/fiap/am/coopfit/services/EmailService.java
+++ src/main/java/br/com/fiap/am/coopfit/services/EmailService.java
@@ -3,10 +3,12 @@ package br.com.fiap.am.coopfit.services;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import br.com.fiap.am.coopfit.domain.Pessoa;
 import br.com.fiap.am.coopfit.domain.Questionario;
 
+@Service
 public interface EmailService {
 	
 	void sendQuestionarioConfirmationEmail(Questionario obj);
